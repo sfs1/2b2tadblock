6e76570555b4a1d040de75c326a69d19f7428fea
billy <billy@DESKTOP-Q4UHRCK.homenet.net>
2025-06-12 08:43:59 +0100

    git test

--- src/main/java/com/sfsarfe/adblock/client/ModConfig.java
+++ src/main/java/com/sfsarfe/adblock/client/ModConfig.java
@@ -6,7 +6,7 @@ import me.shedaniel.autoconfig.annotation.Config;
 @Config(name = "adblock")
 public class ModConfig implements ConfigData
 {
-    public String customRegex = "No regex configured!";
+    public String customRegex = "";
     public boolean enableAdblock = false;
     public boolean autoupdateRegex = false;
     public String webRegex = "";
