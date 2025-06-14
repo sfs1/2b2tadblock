c474da71a07ae7968cf9d999f3f5962140e2d25f
billy <billy@DESKTOP-Q4UHRCK.homenet.net>
2025-06-14 23:46:43 +0100

    nvm we dont need variables (at least not yet)

--- src/main/java/com/sfsarfe/adblock/client/FilterParser.java
+++ src/main/java/com/sfsarfe/adblock/client/FilterParser.java
@@ -6,28 +6,22 @@ import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /*
 Syntax:
 
 # comment
-!IF (condition)
-!ENDIF
 !SERVER (ip, e.g. *.2b2t.org)
 blockthis\([0-9]*\)
+alsoblockthis\(${myvar}\)
 
  */
 public class FilterParser {
     public static List<Filter> parseList(String list) throws ParseException {
         String[] lines = list.split("\n");
         List<Filter> out = new ArrayList<>();
-        boolean process = true;
-        HashMap<String, String> variables = new HashMap<>();
-        // default vars
-        ModConfig config = AutoConfig.getConfigHolder(ModConfig.class).getConfig();
-        variables.put("$AUTOUPDATE", config.autoupdateRegex ? "true" : "false");
-        // dunno what else to put here
-
         for (int i = 0; i < lines.length; i++)
         {
             String line = lines[i];
@@ -45,24 +39,6 @@ public class FilterParser {
             {
                 switch (parts[0])
                 {
-                    case "!IF":
-                    {
-                        // evaluate the expression
-                        String expression = line.substring(parts[0].length());
-
-                        boolean condition = evaluateExpression(expression, server, variables).equals("true");
-
-                        process = !condition;
-                        continue;
-                    }
-                    case "!ENDIF":
-                    {
-                        if (!process)
-                            process = true;
-                        else
-                            throw new ParseException("ENDIF without an IF", i);
-                        continue;
-                    }
                     case "!SERVER":
                     {
                         if (parts.length != 2)
@@ -73,30 +49,17 @@ public class FilterParser {
                 }
             }
 
-            if (process)
-            {
-                try
-                {
-                    "test value".matches(line);
-                    out.add(new Filter(line, server));
-                }
-                catch (Exception e)
-                {
-                    throw new ParseException("Invalid regex `" + line + "`", i);
-                }
+            try {
+                "test value".matches(line);
+                out.add(new Filter(line, server));
+            } catch (Exception e) {
+                throw new ParseException("Invalid regex `" + line + "`", i);
             }
-            else
-                continue;
         }
 
         return out;
     }
 
-    private static String evaluateExpression(String expression, String server, HashMap<String, String> variables) throws ParseException
-    {
-        // i'll get around to this later
-        return "true";
-    }
 
     // idk ill prob rename or redo this
     public record Filter(String blockRegex, String server) {}
